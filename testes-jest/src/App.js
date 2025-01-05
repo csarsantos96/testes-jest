92280cdb79a4af6e2dd7766aa6740bcac20c67e7
Cesar Santos <csar.santos18@gmail.com>
2025-01-04 21:13:32 -0500

    first commit

--- testes-jest/src/App.js
+++ testes-jest/src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [tarefas, setTarefas] = useState([])
+  const [tarefaTemp, setTarefaTemp] = useState('')
+
+function cadastraTarefa(){
+  setTarefas([...tarefas, tarefaTemp])
+  setTarefaTemp ('')
+}
+
   return (
     <div className="App">
-      
+      <input data-testid="campo-tarefa" type="text" value={tarefaTemp} onChange={evento => setTarefaTemp(evento.target.value)} />
+      <button data-testid="btn-cadastrar" onClick={cadastraTarefa} type="button">cadastrar</button>
+      <ul>
+        {tarefas.map(tarefa =>(
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
